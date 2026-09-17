c075b5106735d65b43e8b2e4a773cb64bd3d82c6
owner <saltriokez@DESKTOP-5PS0OD7.localdomain>
2026-09-17 22:29:34 +0300

    Fix: add missing 'recipe' variable to custom libthorvg recipe

--- p4a-recipes/libthorvg/__init__.py
+++ p4a-recipes/libthorvg/__init__.py
@@ -1,12 +1,21 @@
 from pythonforandroid.recipe import Recipe
 
+
 class LibThorvgRecipe(Recipe):
     name = 'libthorvg'
     version = '1.0.0'
-    # Пустой рецепт: ничего не делаем
+
+    def should_build(self, arch):
+        # Говорим p4a: не собирай эту библиотеку
+        return False
+
     def build_arch(self, arch):
+        # Ничего не делаем
         pass
+
     def get_recipe_env(self, arch, **kwargs):
         return {}
-    def should_build(self, arch):
-        return False
+
+
+# Именно этой переменной ждёт p4a
+recipe = LibThorvgRecipe()
